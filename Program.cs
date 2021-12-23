28389d3d913278ebfca197619ab597f3893da5bc
naumenkoff1337 <naumenkoff1337@gmail.com>
2021-12-23 03:57:49 +0300

    Убрал создание мусорного файла, было чисто для теста, зачем залил - хз. Есть баги, связанные с пропуском тасков - может пропуститься просмотр истории либо выполниться и так со всем остальным, позже исправлю...

--- Program.cs
+++ Program.cs
@@ -2,107 +2,125 @@
 using System.Collections.Generic;
 using System.Timers;
 
-namespace RegistrySearcher
+namespace RegistrySearcher;
+
+public static class Program
 {
-    public class Program
+    private static readonly List<string> BlackFilter = new();
+
+    private static void Main()
     {
-        private static readonly List<string> BlackFilter = new() {"windowsselfhost", "edge", "google"};
+        Console.Title = "Simple Registry Searcher";
+        CheckHistory();
+        StartSearch();
+        ExitOrContinueExectuion();
+    }
 
-        private static void Main()
+    private static void StartSearch()
+    {
+        var programName = GetUserVirtualInput("Введите что-то, чтобы найти кое-что > ", ConsoleColor.Red).ToLower();
+        var registrySearcher = new RegistrySearcher(programName, BlackFilter);
+        var multithreaded =
+            GetUserPhysicalInput("Нажмите Y чтобы поиск был многопоточным, иначе он будет однопоточным > ",
+                ConsoleKey.Y, ConsoleColor.Red);
+        var excludedKeys =
+            GetUserVirtualInput($"Введите нежелательные для поиска разделы реестра, сейчас их {BlackFilter.Count} > ",
+                ConsoleColor.Red);
+        AddExcludedKeys(excludedKeys);
+        PrintElapsedTime(registrySearcher);
+        var resultMessage = registrySearcher.DoWork(multithreaded
+            ? RegistrySearcher.SearchType.MultiThread
+            : RegistrySearcher.SearchType.SingleThread);
+        PrintColoredLine(resultMessage, ConsoleColor.Green);
+        SaveResultInFile(registrySearcher);
+    }
+
+    private static void AddExcludedKeys(string keys)
+    {
+        if (string.IsNullOrEmpty(keys)) return;
+        var blackKeys = keys.Split(' ');
+        foreach (var key in blackKeys)
         {
-            Console.Title = "Simple Registry Searcher";
-            CheckHistory();
-            StartSearch();
-            ExitOrContinueExectuion();
+            if (BlackFilter.Contains(key)) return;
+            BlackFilter.Add(key);
         }
+    }
 
-        private static void StartSearch()
-        {
-            var programName = GetUserVirtualInput("Введите что-то, чтобы найти кое-что > ",
-                ConsoleColor.Red).ToLower();
-            var registrySearcher = new RegistrySearcher(programName, BlackFilter);
-            var multithreaded = GetUserPhysicalInput("Нажмите Y чтобы поиск был многопоточным, иначе он будет однопоточным: ", ConsoleKey.Y);
-            PrintElapsedTime(registrySearcher);
-            var resultMessage = registrySearcher.DoWork(multithreaded ? RegistrySearcher.SearchType.MultiThread : RegistrySearcher.SearchType.SingleThread);
-            PrintColoredLine(resultMessage, ConsoleColor.Green);
-            //SaveResultInFile(registrySearcher);
-        }
+    private static void CheckHistory()
+    {
+        if (RegistrySearcher.History.Count is 0) return;
+        if (GetUserPhysicalInput("Если вы хотите просмотреть историю найденных совпадений, нажмите Y > ",
+                ConsoleKey.Y, ConsoleColor.Red) is false) return;
+        foreach (var workResult in RegistrySearcher.History) PrintColoredLine(workResult, ConsoleColor.Green);
+    }
 
-        private static void CheckHistory()
-        {
-            if (RegistrySearcher.History.Count is 0) return;
-            if (GetUserPhysicalInput("Если вы хотите просмотреть историю найденных совпадений, нажмите Y > ",
-                ConsoleKey.Y) is false) return;
-            foreach (var workResult in RegistrySearcher.History) PrintColoredLine(workResult, ConsoleColor.Green);
-        }
-
-        private static void ExitOrContinueExectuion()
-        {
-            if (GetUserPhysicalInput(
+    private static void ExitOrContinueExectuion()
+    {
+        if (GetUserPhysicalInput(
                 "Нажмите Enter что бы выйти или любую другую клавишу чтобы продолжить выполнение программы с начала > ",
-                ConsoleKey.Enter) is true) return;
-            Main();
-        }
+                ConsoleKey.Enter, ConsoleColor.Red) is true) return;
+        Main();
+    }
 
-        private static void ClearLine(int cursorTop)
-        {
-            Console.MoveBufferArea(0, cursorTop, Console.BufferWidth, 1, Console.BufferWidth, cursorTop, ' ',
-                Console.ForegroundColor, Console.BackgroundColor);
-        }
+    private static void ClearLine(int cursorTop)
+    {
+        Console.MoveBufferArea(0, cursorTop, Console.BufferWidth, 1, Console.BufferWidth, cursorTop, ' ',
+            Console.ForegroundColor, Console.BackgroundColor);
+    }
 
-        private static void PrintElapsedTime(RegistrySearcher rs)
+    private static void PrintElapsedTime(RegistrySearcher rs)
+    {
+        var t = new Timer { Interval = 25 };
+        var cursorTop = Console.CursorTop;
+        var startTime = DateTime.Now;
+        t.Elapsed += (a, _) =>
         {
-            var t = new Timer {Interval = 25};
-            var cursorTop = Console.CursorTop;
-            var startTime = DateTime.Now;
-            t.Elapsed += (a, _) =>
+            Console.SetCursorPosition(0, cursorTop);
+            ClearLine(cursorTop);
+            if (rs.Finished)
             {
-                Console.SetCursorPosition(0, cursorTop);
-                ClearLine(cursorTop);
-                if (rs.Finished)
-                {
-                    (a as Timer)?.Close();
-                    return;
-                }
+                (a as Timer)?.Close();
+                return;
+            }
 
-                Console.Write($"Прошло {(DateTime.Now - startTime).TotalMilliseconds / 1000:F} сек.");
-            };
-            t.Start();
-        }
+            Console.Write($"Прошло {(DateTime.Now - startTime).TotalMilliseconds / 1000:F} сек.");
+        };
+        t.Start();
+    }
 
-        private static void SaveResultInFile(RegistrySearcher rs)
-        {
-            if (GetUserPhysicalInput("Если вы хотите сохранить полученный результат на рабочий стол, нажмите Y > ",
-                ConsoleKey.Y) is false) return;
-            var result = rs.SaveResultIntoFile();
-            PrintColoredLine(result, ConsoleColor.Red);
-        }
+    private static void SaveResultInFile(RegistrySearcher rs)
+    {
+        if (GetUserPhysicalInput("Если вы хотите сохранить полученный результат на рабочий стол, нажмите Y > ",
+                ConsoleKey.Y, ConsoleColor.Red) is false) return;
+        var result = rs.SaveResultIntoFile();
+        PrintColoredLine(result, ConsoleColor.Red);
+    }
 
-        private static string GetUserVirtualInput(string message, ConsoleColor color)
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(message);
-            Console.ForegroundColor = color;
-            var input = Console.ReadLine();
-            Console.ResetColor();
-            return input;
-        }
+    private static string GetUserVirtualInput(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(message);
+        Console.ForegroundColor = color;
+        var input = Console.ReadLine();
+        Console.ResetColor();
+        return input;
+    }
 
-        private static bool GetUserPhysicalInput(string message, ConsoleKey targetKey)
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(message);
-            var pressed = Console.ReadKey();
-            Console.WriteLine();
-            Console.ResetColor();
-            return pressed.Key == targetKey;
-        }
+    private static bool GetUserPhysicalInput(string message, ConsoleKey targetKey, ConsoleColor color)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(message);
+        Console.ForegroundColor = color;
+        var pressed = Console.ReadKey();
+        Console.ResetColor();
+        Console.WriteLine();
+        return pressed.Key == targetKey;
+    }
 
-        private static void PrintColoredLine(string message, ConsoleColor color)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
+    private static void PrintColoredLine(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
     }
 }
--- RegistrySearcher.cs
+++ RegistrySearcher.cs
@@ -7,166 +7,141 @@ using System.Threading;
 using Microsoft.Win32;
 using Newtonsoft.Json;
 
-namespace RegistrySearcher
+namespace RegistrySearcher;
+
+public class RegistrySearcher
 {
-    public class RegistrySearcher
+    public enum SearchType
     {
-        public enum SearchType
-        {
-            SingleThread,
-            MultiThread
-        }
+        SingleThread,
+        MultiThread
+    }
 
-        public static List<string> History = new();
-        private IReadOnlyCollection<string> BlackList { get; }
-        private string ProgramName { get; }
-        private List<WorkResult> FoundMatches { get; set; }
-        private TimeSpan SearchTime { get; set; }
-        private int FailedKeyAccess { get; set; }
-        public bool Finished { get; set; }
-        public string Result { get; set; }
+    public static List<string> History = new();
 
-        public RegistrySearcher(string programName, IReadOnlyCollection<string> blackList)
-        {
-            ProgramName = programName;
-            BlackList = blackList;
-            FoundMatches = new List<WorkResult>();
-        }
+    public RegistrySearcher(string programName, IReadOnlyCollection<string> blackList)
+    {
+        ProgramName = programName;
+        BlackList = blackList;
+        FoundMatches = new List<WorkResult>();
+    }
 
-        public async void CrateDeletionFile()
+    private IReadOnlyCollection<string> BlackList { get; }
+    private string ProgramName { get; }
+    private List<WorkResult> FoundMatches { get; set; }
+    private TimeSpan SearchTime { get; set; }
+    private int FailedKeyAccess { get; set; }
+    public bool Finished { get; set; }
+    public string Result { get; set; }
+
+    public string DoWork(SearchType searchType)
+    {
+        return searchType == SearchType.SingleThread ? StartSinglethreadedSearch() : StartMultithreadedSearch();
+    }
+
+    private string StartSinglethreadedSearch()
+    {
+        var startTime = DateTime.Now;
+        var registrySearcher = new Thread(() =>
         {
-            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
-                $"Удаление веток реестра {DateTime.Now.ToLocalTime().ToString(CultureInfo.CurrentCulture).Replace(':', '-')}.reg");
-            using var fileStream = File.CreateText(path);
-            await fileStream.WriteLineAsync("Windows Registry Editor Version 5.00");
-            foreach (var match in FoundMatches)
+            FindMatches(RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64));
+            FindMatches(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64));
+        });
+        registrySearcher.Start();
+        registrySearcher.Join();
+        return GetResult(startTime);
+    }
+
+    private string StartMultithreadedSearch()
+    {
+        var startTime = DateTime.Now;
+        CreateSearchingThread(RegistryHive.CurrentUser);
+        CreateSearchingThread(RegistryHive.LocalMachine).Join();
+        return GetResult(startTime);
+    }
+
+    private Thread CreateSearchingThread(RegistryHive registryHive)
+    {
+        var searchThread = new Thread(() =>
+        {
+            using var registryKey64 = RegistryKey.OpenBaseKey(registryHive, RegistryView.Registry64);
+            FindMatches(registryKey64);
+        });
+        searchThread.Start();
+        return searchThread;
+    }
+
+    private void FindMatches(RegistryKey registryKey)
+    {
+        foreach (var keyName in registryKey.GetSubKeyNames())
+            try
             {
-                try
-                {
-                   await fileStream.WriteLineAsync($"[{match.RegistryKey}]");
-                   await fileStream.WriteLineAsync($"\"{match.ValueName}\"=hex:00");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            await fileStream.FlushAsync();
-        }
-
-        public string DoWork(SearchType searchType)
-        {
-            return searchType == SearchType.SingleThread ? StartSinglethreadedSearch() : StartMultithreadedSearch();
-        }
-
-        private string StartSinglethreadedSearch()
-        {
-            var startTime = DateTime.Now;
-            var registrySearcher = new Thread(() =>
-            {
-                FindMatches(RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64));
-                FindMatches(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64));
-            });
-            registrySearcher.Start();
-            registrySearcher.Join();
-            return GetResult(startTime);
-        }
-
-        private string StartMultithreadedSearch()
-        {
-            var startTime = DateTime.Now;
-            CreateSearchingThread(RegistryHive.CurrentUser);
-            CreateSearchingThread(RegistryHive.LocalMachine).Join();
-            return GetResult(startTime);
-        }
-
-        private Thread CreateSearchingThread(RegistryHive registryHive)
-        {
-            var searchThread = new Thread(() =>
-            {
-                using var registryKey64 = RegistryKey.OpenBaseKey(registryHive, RegistryView.Registry64);
-                FindMatches(registryKey64);
-            });
-            searchThread.Start();
-            return searchThread;
-        }
-
-        private void FindMatches(RegistryKey registryKey)
-        {
-            foreach (var keyName in registryKey.GetSubKeyNames())
-            {
-                try
-                {
-                    using var regKey = registryKey.OpenSubKey(keyName);
-                    if (regKey is null) continue;
-                    var valueNames = regKey.GetValueNames();
-                    if (regKey.SubKeyCount is 0)
+                using var regKey = registryKey.OpenSubKey(keyName);
+                if (regKey is null) continue;
+                var valueNames = regKey.GetValueNames();
+                if (regKey.SubKeyCount is 0)
+                    foreach (var valueName in valueNames)
                     {
-                        foreach (var valueName in valueNames)
+                        var value = regKey.GetValue(valueName) as string;
+                        if (valueName.ToLower().Contains(ProgramName) ||
+                            value != null && value.ToLower().Contains(ProgramName))
                         {
-                            var value = regKey.GetValue(valueName) as string;
-                            if (valueName.ToLower().Contains(ProgramName) ||
-                                value != null && value.ToLower().Contains(ProgramName))
+                            var workResult = new WorkResult
                             {
-                                var workResult = new WorkResult
-                                {
-                                    RegistryKey = regKey.Name,
-                                    ValueName = valueName,
-                                    Value = value
-                                };
-                                FoundMatches.Add(workResult);
-                            }
+                                RegistryKey = regKey.Name,
+                                ValueName = valueName,
+                                Value = value
+                            };
+                            FoundMatches.Add(workResult);
                         }
                     }
-                    else FindMatches(regKey);
-                }
-                catch
-                {
-                    FailedKeyAccess++;
-                }
+                else FindMatches(regKey);
             }
-        }
+            catch
+            {
+                FailedKeyAccess++;
+            }
+    }
 
-        private string GetResult(DateTime startTime)
-        {
-            SearchTime = DateTime.Now - startTime;
-            Filter();
-            Finished = true;
-            Result = JsonConvert.SerializeObject(FoundMatches, Formatting.Indented)
-                     + Environment.NewLine +
-                     $"Найдено {FoundMatches.Count} вхождений за {SearchTime.TotalMilliseconds / 1000:F} сек. Количество необработанных ключей реестра: '{FailedKeyAccess}'.";
-            History.Add(Result);
-            return Result;
-        }
+    private string GetResult(DateTime startTime)
+    {
+        SearchTime = DateTime.Now - startTime;
+        Filter();
+        Finished = true;
+        Result = JsonConvert.SerializeObject(FoundMatches, Formatting.Indented)
+                 + Environment.NewLine +
+                 $"Найдено {FoundMatches.Count} вхождений за {SearchTime.TotalMilliseconds / 1000:F} сек. Количество необработанных ключей реестра: '{FailedKeyAccess}'.";
+        History.Add(Result);
+        return Result;
+    }
 
-        private void Filter()
-        {
-            var foundMatches = (
-                from t in FoundMatches
-                let registryKey = t.RegistryKey.ToLower()
-                where !BlackList.Any(registryKey.Contains)
-                select t).ToList();
-            FoundMatches.Clear();
-            FoundMatches = foundMatches;
-        }
+    private void Filter()
+    {
+        var foundMatches = (
+            from t in FoundMatches
+            let registryKey = t.RegistryKey.ToLower()
+            where !BlackList.Any(registryKey.Contains)
+            select t).ToList();
+        FoundMatches.Clear();
+        FoundMatches = foundMatches;
+    }
 
-        public string SaveResultIntoFile()
-        {
-            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
-                $"Результат сканирования {DateTime.Now.ToLocalTime().ToString(CultureInfo.CurrentCulture).Replace(':', '-')}.json");
-            using var fileStream = File.CreateText(path);
-            fileStream.Write(Result);
-            fileStream.Flush();
-            return $"Результат сканирования сохранен в \"{path}\".";
-        }
+    public string SaveResultIntoFile()
+    {
+        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+            $"Результат сканирования {DateTime.Now.ToLocalTime().ToString(CultureInfo.CurrentCulture).Replace(':', '-')}.json");
+        using var fileStream = File.CreateText(path);
+        fileStream.Write(Result);
+        fileStream.Flush();
+        return $"Результат сканирования сохранен в \"{path}\".";
+    }
 
-        public class WorkResult
-        {
-            [JsonProperty("Registry Key")] public string RegistryKey { get; set; }
+    public class WorkResult
+    {
+        [JsonProperty("Registry Key")] public string RegistryKey { get; set; }
 
-            [JsonProperty("Value Name")] public string ValueName { get; set; }
+        [JsonProperty("Value Name")] public string ValueName { get; set; }
 
-            [JsonProperty("Value")] public string Value { get; set; }
-        }
+        [JsonProperty("Value")] public string Value { get; set; }
     }
 }
